5a0f8eb661f6e07a083eb1320f07123e1c53f1a8
fares <fares12yaseer45@gmail.com>
2022-10-26 23:31:34 +0200

    unimportant modifie

--- src/components/Join/Form.js
+++ src/components/Join/Form.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const Form = () => {
+
+    const drag = document.getElementById('grag')
+    const space = document.querySelector('.space')
+
+
+    const setimage = () => {
+        const val = drag.value
+        space.value = val
+    }
     return (
-        <div>Form</div>
+        <div>
+            <div className='dropper'>
+            <input type='file' id='grag' />
+            <div className='space'></div>
+            <button onClick={setimage}>see</button>
+            </div>
+        </div>
     )
 }
 
