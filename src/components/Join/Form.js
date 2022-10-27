fa9b879c2158ed3bac1d30e9a3f8dd1702daa105
fares <fares12yaseer45@gmail.com>
2022-10-27 04:35:43 +0200

    finish style the join section

--- src/components/Join/Form.js
+++ src/components/Join/Form.js
@@ -6,6 +6,18 @@ const Form = () => {
         <div className='form-container'>
             <DD />
             <span className='steak'></span>
+            <div className='form'>
+                <form>
+                <div className='title'>Your Info</div>
+                
+                <label for='name'>Name: <input placeholder='name' id='name' type='text' /></label>
+                
+                <label for='pass'>Password: <input placeholder='password' id='pass' typy='password' /></label>
+                <label for='email'>Email: <input placeholder='Email...' id='email' typy='email' /></label>
+                <label for='number'>Number: <input placeholder='Number...' id='number' typy='number' /></label>
+                <button className='submit-btn' type='submit'>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
