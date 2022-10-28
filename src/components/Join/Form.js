d79d3c4459b443d563717bc4db9b928dc3ebbf3d
fares <fares12yaseer45@gmail.com>
2022-10-28 16:38:24 +0200

    write teh database logic code and found an one error in components re-rendring

--- src/components/Join/Form.js
+++ src/components/Join/Form.js
@@ -1,29 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import DD from './Drag&Drop/DD'
+import Lognin from '../Login&Signup/Lognin'
+import {db} from '../firebase-config'
+import {addDoc, collection, getDocs} from 'firebase/firestore'
 
 const Form = () => {
-    const [data , setData] = useState([])
+    const [email , setEmail] = useState("")
+    const [number , setNubmer] = useState(0)
 
-    const PassInfo = (val) => {
-        setData(val)
+
+    const [imageData , setImageData] = useState([])
+    const [UserName , getUserName] = useState("")
+    const [UserPass , getUserPass] = useState("")
+
+
+    const[users , setUsers] = useState([])
+    const userCollectionRef = collection(db , "users")
+
+    const CreateUser = async () => {
+        await addDoc(userCollectionRef , {
+            image: imageData,
+            name: UserName,
+            password: UserPass,
+            email: email,
+            number: Number(number)
+        })
     }
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(userCollectionRef)
+            setUsers(data.docs.map((doc) => ({...doc.data, id: doc.id})))
+        }
+        getUsers()
+    }, [])
+
+    const PassData = (val) => {
+        setImageData(val)
+    }
+
+    const Name = (n) => {
+        getUserName(n)
+    }
+
+    const Pass = (p) => {
+        getUserPass(p)
+    }
+
+
+
 
     return (
         <div className='form-container'>
-            <DD PassInfo={PassInfo} />
+            <DD PassData={PassData} />
             <span className='steak'></span>
             <div className='form'>
                 <form>
                 <div className='title'>Your Info</div>
                 
-                <label for='name'>Name: <input placeholder='name' id='name' type='text' /></label>
+                <label for='name'>Name: <input placeholder='name' value={UserName} id='name' type='text' /></label>
                 
-                <label for='pass'>Password: <input placeholder='password' id='pass' type='password' /></label>
-                <label for='email'>Email: <input placeholder='Email...' id='email' type='email' /></label>
-                <label for='number'>Number: <input placeholder='Number...' id='number' type='text' /></label>
-                <button className='submit-btn' type='submit'>Submit</button>
+                <label for='pass'>Password: <input placeholder='password' value={UserPass} id='pass' type='password' /></label>
+
+                <label for='email'>Email: <input onChange={(event) => setEmail(event.target.value)} placeholder='Email...' id='email' type='email' /></label>
+
+                <label for='number'>Number: <input onChange={(event) => setNubmer(event.target.value)} placeholder='Number...' id='number' type='text' /></label>
+
+                <button onClick={CreateUser} className='submit-btn' type='submit'>Submit</button>
+
                 </form>
             </div>
+
+            <div className='hidden-area'><Lognin Pass={Pass} Name={Name} className='hidden' /></div>
         </div>
     )
 }
--- src/components/Login&Signup/Lognin.js
+++ src/components/Login&Signup/Lognin.js
@@ -1,27 +1,39 @@
-import React, {useState , useEffect} from 'react'
-import {db} from '/src/firebase-config'
-import {collection , getDoc , addDoc} from 'firebase/firestore'
+import React, {useState} from 'react'
+// import {db} from '/src/firebase-config'
+// import {collection , getDoc , addDoc} from 'firebase/firestore'
 
-const Lognin = () => {
+const Lognin = ({Name , Pass}) => {
 
     const [newName , setNewName] = useState("")
-    const [password , setpassword] = useState(0)
+    const [password , setpassword] = useState("")
 
-    const [users , setUsers] = useState([])
-    const UserCollection = collection(db , "users")
+    // const [users , setUsers] = useState([])
+    // const UserCollection = collection(db , "users")
     
 
-    useEffect(() => {
-        const getUsers = async () => {
-            const data = await getDoc(UserCollection)
-            setUsers(data.docs.map((doc) => ({...doc.data() , id: doc.id})))
-        }
-        getUsers()
-    }, [])
+    // useEffect(() => {
+    //     const getUsers = async () => {
+    //         const data = await getDoc(UserCollection)
+    //         setUsers(data.docs.map((doc) => ({...doc.data() , id: doc.id})))
+    //     }
+    //     getUsers()
+    // }, [])
 
-    const CreateUser = async () => {
-        await addDoc(UserCollection , {name: newName , password: password})
+    // const CreateUser = async () => {
+    //     await addDoc(UserCollection , {name: newName , password: password})
+    // }
+
+    const GetName = () => {
+        Name(newName)
     }
+
+    GetName()
+
+    const getPass = () => {
+        Pass(password)
+    }
+
+    getPass()
 
     return (
     <div className='form'>
@@ -38,7 +50,7 @@ const Lognin = () => {
         onChange={(event) => {setpassword(event.target.value)}}
         />
         <div className='btns'>
-        <button type='submit' onClick={CreateUser} className='submit-button'>
+        <button type='submit' className='submit-button'>
             <a href='../Join/MainPage.js'>Submit</a>
         </button>
         </div>
