707e79041c310b4ffb589200e11321dd436a549f
fares <fares12yaseer45@gmail.com>
2022-11-11 10:37:27 +0200

    start create the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React  from 'react'
+import {Routes , Route} from 'react-router-dom'
+import MainPage from './components/Join/MainPage'
 import Section from './components/user-page/MainSection/Section'
 
 function App() {
   return (
     <div className='app'>
-      <Section />
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+        <Route path='/UserPage' element={<Section />}/>
+      </Routes>
     </div>
   );
 }
